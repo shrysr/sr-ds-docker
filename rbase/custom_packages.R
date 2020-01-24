0277ec340a2a4f5de0bea7f1477f4e2a3883eccd
Shreyas Ragavan <sr@eml.cc>
2020-01-23 18:39:54 -0800

    Added some packages to custom package list

--- rbase/custom_packages.R
+++ rbase/custom_packages.R
@@ -1,8 +1,11 @@
 #Script for common package installation on MatrixDS docker image
 PKGS <- c(
-      "tidyverse", "mapproj", "maps", "genius"
+      "tidyverse", "mapproj", "maps", "genius", "shinycssloaders", "gmailr"
 )
 
 install.packages(PKGS, dependencies = TRUE)
+
+# These packages are sometimes not available for the current R version
+# , and therefore installed directly from github
 devtools::install_github("tidyverse/googlesheets4", dependencies = TRUE)
 devtools::install_github("tidyverse/googletrendsR", dependencies = TRUE)
